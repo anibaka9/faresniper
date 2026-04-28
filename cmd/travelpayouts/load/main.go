40b61aa47765d2804d978b04d0cf9b75d9524d8c
Aleskei Budkin <anibaka9@gmail.com>
2026-04-28 10:13:38 +0600

    removed data and saved request date #6

--- cmd/travelpayouts/load/main.go
+++ cmd/travelpayouts/load/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/anibaka9/faresniper/internal/travelpayouts"
 )
@@ -17,8 +18,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fileName := fmt.Sprintf("data/travelpayouts/%s.json", origin)
+	currentDay := time.Now().Local().Format("2006-01-02")
+	fileName := fmt.Sprintf("data/travelpayouts/%s_%s.json", origin, currentDay)
 	if err := os.WriteFile(fileName, data, 0o666); err != nil {
 		log.Fatal("coudnt write data", err)
 	}
